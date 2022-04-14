2ceb1fd193640df053b867332dd6aaab7c277e93
warner hurtado laguna <warner3898@gmail.com>
2022-04-14 16:37:58 -0600

    IU ready, pending endpoints

--- src/components/homeScreen/Home.js
+++ src/components/homeScreen/Home.js
@@ -1,15 +1,54 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../../auth/authContext';
 import { types } from '../../types/types';
+import useSpeechToText from 'react-hook-speech-to-text';
+import { Predictions } from './Predictions';
 
 export const Home = () => {
 
-  const { dispatch } = useContext( AuthContext );
+  const { dispatch } = useContext(AuthContext);
+
+  const [nameModel, setNameModel] = useState(null);
+  const [searching, setSearching] = useState(null);
 
   const handleLogout = () => {
-  
-    dispatch({ type: types.logout });  
-  }; 
+
+    dispatch({ type: types.logout });
+  };
+
+  const {
+    error,
+    //interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    useLegacyResults: false
+  });
+
+  useEffect(() => {
+
+    setNameModel(null);
+
+    setSearching(results[results.length - 1]?.transcript);
+
+    if (results[results.length - 1]) {
+      results[results.length - 1]?.transcript.includes('aguacate') && setNameModel('avocado_model');
+      results[results.length - 1]?.transcript.includes('carro') && setNameModel('car_model');
+      results[results.length - 1]?.transcript.includes('masa') && setNameModel('mass_model');
+      results[results.length - 1]?.transcript.includes('bitcoin') && setNameModel('bitcoin_model');
+      results[results.length - 1]?.transcript.includes('cancer') && setNameModel('breast_cancer');
+      results[results.length - 1]?.transcript.includes('ventas') && setNameModel('rossman_sales_model');
+      results[results.length - 1]?.transcript.includes('caballo') && setNameModel('horse_power_calculator');
+      results[results.length - 1]?.transcript.includes('salario') && setNameModel('salary_years_calculator');
+    }
+
+  }, [results])
+
+
+  if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
 
   return (
     <div className='animate__animated animate__bounceInRight'>
@@ -19,18 +58,38 @@ export const Home = () => {
 
           <ul className="nav justify-content-end">
             <li className="nav-item">
-              <a className="nav-link" href="/">Link</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href='/'>Disabled</a>
-            </li>
-            <li className="nav-item">
-              <label className="nav-link" onClick={ handleLogout } style={{cursor: 'pointer'}}>Logout</label>
+              <label className="nav-link" onClick={handleLogout} style={{ cursor: 'pointer' }}>Logout</label>
             </li>
           </ul>
         </div>
       </nav>
-      <h1>Home</h1>
+
+      <div className='container text-center mt-3'>
+        <h1>Recording: {isRecording.toString()}</h1>
+        <button className='btn btn-primary' onClick={isRecording ? stopSpeechToText : startSpeechToText}>
+          {isRecording ? 'Stop Recording' : 'Start Recording'}
+        </button>
+      </div>
+
+      <div className='container mt-5'>
+
+        {
+          nameModel ?
+            <div className='container shadow-lg p-3 mb-5 bg-body rounded mt-5'>
+              <Predictions 
+                nameModel={nameModel} 
+                searching={searching}
+                />
+            </div>
+
+            :
+
+            <div className=" container alert alert-warning text-center" role="alert">
+              Dile a Jarvis con que quieres que te ayude.
+            </div>
+        }
+      </div>
+
     </div>
   )
 }
---
+++ src/components/homeScreen/Predictions.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+const formsList = {
+    car_model: [{ name: 'presentPrice', type: 'number', label: 'Precio presente', error: 'Debe ingresar un precio' }],
+    mass_model: [{ name: 'Weight', type: 'number', label: 'Peso', error: 'Debe ingresar un peso' }],
+    avocado_model: [{ name: '4046', type: 'number', label: 'Total de aguacates con PLU 4046 sold', error: 'Debe ingresar el total de aguacates con PLU 4046 sold' }, { name: '4225', type: 'number', label: 'Total de aguacates con PLU 4225 sold', error: 'Debe ingresar el total de aguacates con PLU 4225 sold' }, { name: '4770', type: 'number', label: 'Total de aguacates con PLU 4770 sold', error: 'Debe ingresar el total de aguacates con PLU 4770 sold' }],
+    bitcoin_model: [{ name: 'open', type: 'number', label: 'Precio apertura', error: 'Debe ingresar un precio' }, { name: 'low', type: 'number', label: 'Precio mas bajo', error: 'Debe ingresar un precio' }, { name: 'high', type: 'number', label: 'Precio mas alto', error: 'Debe ingresar un precio' }],
+    breast_cancer: [{ name: 'perimeter', type: 'number', label: 'Perimetro', error: 'Debe ingresar un perimetro' }, { name: 'area', type: 'number', label: 'Area', error: 'Debe ingresar el area' }, { name: 'radio', type: 'number', label: 'Radio', error: 'Debe ingresar el radio' }],
+    rossman_sales_model: [{ name: 'customers', type: 'number', label: 'Cantidad de clientes', error: 'Debe ingresar una cantidad de clientes' }],
+    horse_power_calculator: [{ name: 'yearSale', type: 'number', label: 'Año de reventa', error: 'Debe ingresar un año' }, { name: 'price', type: 'number', label: 'Precio en miles', error: 'Debe ingresar un precio' }, { name: 'eginie', type: 'number', label: 'Tamaño de la máquina', error: 'Debe ingresar un tamaño' }],
+    salary_years_calculator: [{ name: 'years', type: 'number', label: 'Años de experiencia', error: 'Debe ingresar un año' }]
+};
+
+export const Predictions = ({ nameModel, searching }) => {
+
+    const [formsResult, setFormsResult] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();        
+
+        formsList[nameModel].forEach( (a,index) => {
+            setFormsResult(formsResult.push(Number( e.target[index].value) ));
+        });
+        
+        console.log(formsResult); 
+        
+    };
+
+    return (
+        <>
+            <hr />
+            <div className='container text-center'>
+                <h3>
+                    {searching}	
+                </h3>
+            </div>
+            <div className='container'>
+                <form onSubmit={handleSubmit}>
+
+                    {
+                        formsList[nameModel].map((form) => (
+                            <div className='form-floating mb-3' key={form.name}>
+                                <input 
+                                    type={form.type} 
+                                    className="form-control" 
+                                    id={form.name} 
+                                    placeholder={form.label} 
+                                    name={form.name}/>
+                                <label >{form.label}</label>
+                            </div>
+                        ))
+                    }
+                    <button type='submit' className='btn btn-primary'>
+                        Realizar analisis
+                    </button>
+
+                </form>
+            </div>
+        </>
+    )
+}
